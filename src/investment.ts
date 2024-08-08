c87ab2070c07ad56d767ddc78c39f39606edb91e
skoczylm <skoczylasmikolaj@gmail.com>
2024-08-08 18:34:27 +0200

    Initial commit

---
+++ src/investment.ts
@@ -0,0 +1,26 @@
+class Investment {
+
+  
+    initialAmount: 250000,
+    recurringAmount: 5000,
+    depositInterval: 'monthly',
+    intrestRate: 8,
+    capitalisationInterval: 'yearly',
+    startAge: 32,
+    endAge: 40,
+  
+  greeting: string;
+
+  constructor(
+    initialAmount: number,
+    ) {
+    this.greeting = message;
+
+  }
+
+  greet() {
+    return 'Hello, ' + this.greeting;
+  }
+}
+
+let greeter = new Greeter('world');
